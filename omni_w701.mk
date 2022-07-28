2f860787fa1274d5fd7bb031ed3d65aa3a8673a1
mediateeee <mediateeee@outlook.com>
2022-07-28 13:57:26 +0800

    device: android 7.1 Support

--- omni_w701.mk
+++ omni_w701.mk
@@ -17,16 +17,10 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from w701 device
-$(call inherit-product, device/qibao/w701/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := w701
